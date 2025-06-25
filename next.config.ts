f9e8bdea2f49a25bc22e7ccd4ca1783910854fe2
Mizuto, Kaminakano <mizuto1127no79@gmail.com>
2025-06-25 11:04:37 +0000

    rename out to docs

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import { env } from "process";
 const nextConfig: NextConfig = {
   allowedDevOrigins: [env.REPLIT_DOMAINS.split(",")[0]],
   //basePath: ""
+  distDir: "docs",
   output: "export",
 };
 
