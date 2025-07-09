18fdd6ea796496852eebc0837ebc792c9ccd5c68
Mizuto, Kaminakano <mizuto1127no79@gmail.com>
2025-07-09 04:25:44 +0000

    buildとdevでdistDirをそれぞれ指定する

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 import type { NextConfig } from "next";
 import { env } from "process";
 
+const isExport: boolean = process.env.NEXT_PHASE === "phase-export";
+const isDev: boolean = process.env.NODE_ENV === "development";
+
 const nextConfig: NextConfig = {
   //allowedDevOrigins: [env.REPLIT_DOMAINS.split(",")[0]],
   //basePath: ""
-  distDir: "docs",
-  output: "export",
+  distDir: isExport ? "docs" : ".next",
+  output: isExport ? "export" : undefined,
 };
 
 module.exports = nextConfig;
