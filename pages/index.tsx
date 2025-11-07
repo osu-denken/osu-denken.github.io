5241e6df9564f097e6940111bd6fe1c7cf06b83f
Mizuto, Kaminakano <mizuto1127no79@gmail.com>
2025-11-07 04:50:02 +0000

    fix: terminal size, content

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,12 +10,10 @@ const HomePage: NextPage = () => {
         <h1 className={styles.title}>電子計算研究部</h1>
         <h2 className={styles.bold}>大阪産業大学</h2>
 
-        {/* <p className={styles.description}>
-          パソコン、ネットワーク、ゲーム、プログラミングといった活動をしている大産大のクラブです。
-        </p> */}
-
+        <div className={styles.terminalContainer}>
         <iframe className={styles.terminal} src="./terminal.html"></iframe>
-
+        </div>
+      
         <div className={styles.iconContainer}>
           <a href="https://www.osaka-sandai.ac.jp/club_circle/club/233">
             <Icon
