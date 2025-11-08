e3736f88742d29fff8653848ecceafa73b18188f
Mizuto, Kaminakano <mizuto1127no01@gmail.com>
2025-11-08 11:37:10 +0000

    feat: add About page styles and structure

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,17 +1,19 @@
 import type { NextPage } from "next";
-import styles from "../../styles/Home.module.css";
+import styles from "../../styles/Page.module.css";
 
 const AboutPage : NextPage = () => {
   return (
     <div className={styles.container}>
-      <h1>電研について</h1>
-      <p className={styles.description}>
-        電子計算研究部（通称：電研、Denken）は大阪産業大学のコンピュータ系の部活です。<br />
-        パソコン、ネットワーク、ゲーム、プログラミングといった活動を通じて、技術力の向上や部員同士との交流を深めています。<br />
-        <br />
-        年に一度、パソコンの組立てを行います。<br />
-        また、大学祭 (阪駒祭) では、部員が開発したプログラムやゲーム体験、パソコン組立て体験、コンピュータ/サーバーなどの機器の展示を行っています。<br />
-      </p>
+      <main className={styles.main}>
+        <h1>電研について</h1>
+        <p className={styles.description}>
+          電子計算研究部（通称：電研、Denken）は大阪産業大学のコンピュータ系の部活です。<br />
+          パソコン、ネットワーク、ゲーム、プログラミングといった活動を通じて、技術力の向上や部員同士との交流を深めています。<br />
+          <br />
+          年に一度、パソコンの組立てを行います。<br />
+          また、大学祭 (阪駒祭) では、部員が開発したプログラムやゲーム体験、パソコン組立て体験、コンピュータ/サーバーなどの機器の展示を行っています。<br />
+        </p>
+      </main>
     </div>
   );
 };
