0cca0873cf776754ea5e9f561b0771b27caaa6fc
osu-denken-admin <9510denken@gmail.com>
2025-12-06 08:11:36 +0000

    feat: impl login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,38 +4,31 @@ import type { NextPageWithLayout } from "./api/NextPageWithLayout";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Head from "next/head";
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp, getApp, getApps } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// import { initializeApp, getApp, getApps } from "firebase/app";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: "osu-denken-web.firebaseapp.com",
-  projectId: "osu-denken-web",
-  storageBucket: "osu-denken-web.firebasestorage.app",
-  messagingSenderId: "302700681902",
-  appId: "1:302700681902:web:3dedcf68544f9f00b8eb3f",
-  measurementId: "G-K8FSER9QG2",
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAq6xQfhr6jbRxOK0Ds1y-5NfwZLgZ0U40",
+//   authDomain: "osu-denken-auth.firebaseapp.com",
+//   projectId: "osu-denken-auth",
+//   storageBucket: "osu-denken-auth.firebasestorage.app",
+//   messagingSenderId: "252294046970",
+//   appId: "1:252294046970:web:83f4f732098e613bd2d543"
+// };
 
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
 
-let app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+// let app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-//      const analytics = getAnalytics(app);
-    }
-  }, []);
+//   useEffect(() => {
+//     if (typeof window !== "undefined") {
+// //      const analytics = getAnalytics(app);
+//     }
+//   }, []);
   
   const getLayout =
     Component.getLayout ?? ((page) => (
--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -6,19 +6,23 @@ import { useState, useEffect } from "react";
 const modalStyle: Modal.Styles = {
   overlay: {
     position: "fixed",
+    zIndex: 10,
     top: 0,
     left: 0,
-    backgroundColor: "rgba(0,0,0,0.85)"
+    backgroundColor: "rgba(0,0,0,0.25)"
   },
   content: {
-    position: "absolute",
-    top: "5rem",
-    left: "5rem",
-    right: "5rem",
-    bottom: "5rem",
-    backgroundColor: "white",
+    position: "relative",
+    zIndex: 10,
+    maxWidth: "20rem",
+    minWidth: "16rem",
+    minHeight: "16rem",
+    backgroundColor: "#111111bf",
+    border: "1px solid #23df1e",
+    boxShadow: "0 0 3px #23df1eaa,0 0 6px #23df1eaa",
     borderRadius: "1rem",
-    padding: "1.5rem"
+    padding: "1.5rem",
+    textAlign: "center",
   }
 };
 
@@ -30,6 +34,64 @@ const Navbar: React.FC = () => {
       Modal.setAppElement('.App');
     }
   }, []);
+
+  // onclick
+  const handleLogin = (e: React.MouseEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    //alert("ログイン処理が実行されました。");
+    // https://api.osudenken4dev.workers.dev/user/login POST email password
+    const form = e.currentTarget.form;
+    if (!form) return;
+    const formData = new FormData(form);
+    const username = formData.get("username");
+    const password = formData.get("password");
+
+    fetch("https://api.osudenken4dev.workers.dev/user/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        email: username,
+        password: password
+      })
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log("data:", data);
+        if (data.idToken) {
+          localStorage.setItem("idToken", data.idToken);
+          alert("ログインに成功しました");
+          setIsOpen(false);
+        } else {
+          alert("ログイン失敗: " + data.message);
+        }
+      })
+      .catch(error => {
+        console.error("Error:", error);
+        alert("ログイン中にエラーが発生しました。");
+      });
+  }
+
+  let loginNav = (
+    <li className={styles.rightend}>
+      <a onClick={() => setIsOpen(true)}>ログイン</a>
+    </li>
+  );
+
+  if (typeof window !== 'undefined' && localStorage.getItem("idToken")) {
+    loginNav = (
+      <li className={styles.rightend}>
+        <a onClick={
+          () => {
+            localStorage.removeItem("idToken");
+            alert("ログアウトしました");
+            window.location.reload();
+          }
+        }>ログアウト</a>
+      </li>
+    );
+  }
 
   return (
     <nav className={styles.nav}>
@@ -43,21 +105,38 @@ const Navbar: React.FC = () => {
         <li className={styles.right}>
           <a href="https://osu-denken.github.io/blog/join">入部</a>
         </li>
-        {/* <li className={styles.rightend}>
-          <a onClick={() => setIsOpen(true)}>Login</a>
-        </li> */}
+        {loginNav}
         {/* <li className={styles.right}>
           <a>設定</a>
         </li> */}
       </ul>
-        <Modal isOpen={modalIsOpen} style={modalStyle} onRequestClose={() => setIsOpen(false)}>
-        <button onClick={() => setIsOpen(false)}>閉じる</button>
+      <Modal isOpen={modalIsOpen} style={modalStyle} onRequestClose={() => setIsOpen(false)} className="modalContent" overlayClassName="modalOverlay" closeTimeoutMS={200} >
+        <button style={{
+    position: "absolute",
+    top: "0.5rem",
+    right: "0.5rem",
+    background: "transparent",
+    border: "2px solid #23df1e",
+    color: "#23df1e",
+    fontSize: "1.5rem",
+    width: "2rem",
+    height: "2rem",
+    borderRadius: "0.5rem",
+    cursor: "pointer"
+  }} onClick={() => setIsOpen(false)}>×</button>
+        <h2>ログイン</h2>
         <form method="POST">
+          <h3>学籍番号</h3>
           <input type="text" name="username" /><br />
+          <h3>パスワード</h3>
           <input type="password" name="password" />
           <br />
-          <input type="submit" />
+          <input type="submit" value="ログイン" onClick={handleLogin} />
         </form>
+
+        <p>
+          ログインは大産大の電研部員のみが可能となっております。
+        </p>
       </Modal>
     </nav>
   );
