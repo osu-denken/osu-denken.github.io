784f273ac11111eb69b7664ca8f810588152a803
osu-denken-admin <9510denken@gmail.com>
2025-12-06 08:22:08 +0000

    fix: remove not need comments

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,32 +4,12 @@ import type { NextPageWithLayout } from "./api/NextPageWithLayout";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Head from "next/head";
-// import { useEffect } from "react";
-
-// import { initializeApp, getApp, getApps } from "firebase/app";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAq6xQfhr6jbRxOK0Ds1y-5NfwZLgZ0U40",
-//   authDomain: "osu-denken-auth.firebaseapp.com",
-//   projectId: "osu-denken-auth",
-//   storageBucket: "osu-denken-auth.firebasestorage.app",
-//   messagingSenderId: "252294046970",
-//   appId: "1:252294046970:web:83f4f732098e613bd2d543"
-// };
 
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
 
-// let app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
-
-function MyApp({ Component, pageProps }: AppPropsWithLayout) {
-//   useEffect(() => {
-//     if (typeof window !== "undefined") {
-// //      const analytics = getAnalytics(app);
-//     }
-//   }, []);
-  
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {  
   const getLayout =
     Component.getLayout ?? ((page) => (
     <div className="App">
