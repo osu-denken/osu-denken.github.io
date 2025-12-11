e1e6cb75c93a72507c90adc083ffe2c549d70f91
osu-denken-admin <9510denken@gmail.com>
2025-12-11 06:03:13 +0000

    fix: update footer text and add link to Osaka Sangyo University

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -1,15 +1,16 @@
 import styles from '@styles/Footer.module.css';
+import Link from 'next/link';
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.wrapper}>
-        <h2 className={styles.footerHeading}>電研HP</h2>
+        <h2 className={styles.footerHeading}>電研公式サイト</h2>
 
         <div className={styles.footerColWrapper}>
           <div className={`${styles.footerCol} ${styles.footerCol1}`}>
             <ul className={styles.contactList}>
-              <li>大阪産業大学文化会 電子計算研究部</li>
+              <li><Link href="https://www.osaka-sandai.ac.jp/" target='_blank'>大阪産業大学</Link> 文化会 電子計算研究部</li>
             </ul>
           </div>
 
