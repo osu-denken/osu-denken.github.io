fa1823a334ba42071cc151fedc481ec7df8996f4
kmmz532 <mizuto1127no01@gmail.com>
2026-07-07 15:44:43 +0900

    feat: add WorkCard component with carousel functionality and update dependencies

---
+++ pages/works/WorkCard.tsx
@@ -0,0 +1,61 @@
+import useEmblaCarousel from "embla-carousel-react";
+import styles from "./workcard.module.css"
+import Autoplay from "embla-carousel-autoplay";
+
+type Props = {
+  work: {
+    id: number;
+    title: string;
+    overview?: string;
+    description?: string;
+    images: string[];
+    link?: string;
+  };
+};
+
+const WorkCard = ({ work }: Props) => {
+  // ループと、ドラッグでの切り替えを有効に
+  const [emblaRef] = useEmblaCarousel({ loop: true }, [
+    Autoplay({ delay: 3000, stopOnInteraction: false })
+  ]);
+  
+  return (
+    <div className={styles.card}>
+      <h3 className={styles.workTitle}>
+        {work.link ? (
+          <a href={work.link} target="_blank" rel="noopener noreferrer" className={styles.workLink}>
+            {work.title}
+          </a>
+        ) : (
+          work.title
+        )}
+      </h3>
+      {work.overview && <p className={styles.workDescription}>{work.overview}</p>}
+
+      {/* 作品の中の画像カルーセル */}
+      <div className={styles.embla} ref={emblaRef}>
+        <div className={styles.emblaContainer}>
+          {work.images.map((src, index) => (
+            <div className={styles.emblaSlide} key={index}>
+              <img src={src} alt={`${work.title}-${index}`} className={styles.workImage} />
+            </div>
+          ))}
+        </div>
+      </div>
+      
+      {/* 作品の情報 */}
+        <div className={styles.workInfo}>
+        {work.description && (
+          <p dangerouslySetInnerHTML={{ __html: work.description }} />
+        )}
+        {work.link && (
+          <a href={work.link} target="_blank" rel="noopener noreferrer" className={styles.workLink}>
+            作品ページへ
+          </a>
+        )}
+        </div>
+    </div>
+  );
+};
+
+export default WorkCard;
