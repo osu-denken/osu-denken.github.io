99c7c6e3ec271a505db4103095ee4b7fa87b34df
kmmz532 <mizuto1127no01@gmail.com>
2026-07-24 22:42:36 +0900

    feat: add OGP

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,8 +15,30 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
     <div className="App">
       <Head>
         <title>大阪産業大学 電子計算研究部</title>
-        <meta name="description" content="" />
+        <meta name="description" content="大阪産業大学 電子計算研究部の公式サイト。作品やブログ、活動内容を紹介しています。" />
         <link rel="icon" href="/favicon.ico" />
+
+        {/* OGP (Open Graph Protocol) */}
+        <meta property="og:site_name" content="大阪産業大学 電子計算研究部" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="大阪産業大学 電子計算研究部" />
+        <meta
+          property="og:description"
+          content="大阪産業大学 電子計算研究部の公式サイト。作品やブログ、活動内容を紹介しています。"
+        />
+        <meta property="og:url" content="https://osu-denken.github.io/" />
+        <meta property="og:image" content="https://osu-denken.github.io/icon.png" />
+        <meta property="og:locale" content="ja_JP" />
+
+        {/* Twitter Card */}
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@osu_denken" />
+        <meta name="twitter:title" content="大阪産業大学 電子計算研究部" />
+        <meta
+          name="twitter:description"
+          content="大阪産業大学 電子計算研究部の公式サイト。作品やブログ、活動内容を紹介しています。"
+        />
+        <meta name="twitter:image" content="https://osu-denken.github.io/icon.png" />
         <link
           rel="alternate"
           type="application/rss+xml"
